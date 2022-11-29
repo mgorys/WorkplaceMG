95000c87a97364ab425395a25bfdfa5048faf195
gogo <gorycho@interia.pl>
2022-11-29 12:28:02 +0100

    workplace tables/seeder

--- WorkplaceMG/Entities/WorkplaceDbContext.cs
+++ WorkplaceMG/Entities/WorkplaceDbContext.cs
@@ -2,22 +2,44 @@
 
 namespace WorkplaceMG.Entities
 {
-    
+
     public class WorkplaceDbContext : DbContext
     {
         public WorkplaceDbContext(DbContextOptions<WorkplaceDbContext> options) : base(options)
         {
         }
-        //private string _connectionString =
-        //   "Server=.;Database=WorkplaceDb;Trusted_Connection=True;";
+
         public DbSet<Workplace> Workplaces { get; set; }
         public DbSet<Employee> Employees { get; set; }
         public DbSet<Reservation> Reservations { get; set; }
         public DbSet<Equipment> Equipments { get; set; }
         public DbSet<EquipmentForWorkplace> EquipmentForWorkplaces { get; set; }
-        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        //{
-        //    optionsBuilder.UseSqlServer(_connectionString);
-        //}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>().HasData(
+                    new Employee { Id =1, FirstName = "Antony", LastName = "Hopkins" },
+                    new Employee { Id =2, FirstName = "Tom", LastName = "Hanks" }
+                );
+            modelBuilder.Entity<Equipment>().HasData(
+                    new Equipment { Id = 1, Type = "Headphones", StockCount = 3 },
+                    new Equipment { Id = 2, Type = "Keyboard", StockCount = 3 },
+                    new Equipment { Id = 3, Type = "Mouse", StockCount = 3 }
+                );
+            modelBuilder.Entity<Workplace>().HasData(
+                    new Workplace { Id =1, Floor = 0, Room=1,Table=1 },
+                    new Workplace { Id =2, Floor = 0, Room=1,Table=2 },
+                    new Workplace { Id =3, Floor = 0, Room=2,Table=1 },
+                    new Workplace { Id =4, Floor = 1, Room=1,Table=1 }
+                );
+            modelBuilder.Entity<EquipmentForWorkplace>().HasData(
+                    new EquipmentForWorkplace { Id = 1, IdWorkplace = 1, IdEquipment =1 },
+                    new EquipmentForWorkplace { Id = 2, IdWorkplace = 1, IdEquipment =2 },
+                    new EquipmentForWorkplace { Id = 3, IdWorkplace = 2, IdEquipment =1 },
+                    new EquipmentForWorkplace { Id = 4, IdWorkplace = 2, IdEquipment =2 },
+                    new EquipmentForWorkplace { Id = 5, IdWorkplace = 3, IdEquipment =1 },
+                    new EquipmentForWorkplace { Id = 6, IdWorkplace = 4, IdEquipment =2 }
+                );
+        }
     }
 }
---
+++ WorkplaceMG/Migrations/20221129111313_seeder.cs
@@ -0,0 +1,133 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WorkplaceMG.Migrations
+{
+    public partial class seeder : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "Id", "FirstName", "LastName" },
+                values: new object[,]
+                {
+                    { 1, "Antony", "Hopkins" },
+                    { 2, "Tom", "Hanks" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Equipments",
+                columns: new[] { "Id", "StockCount", "Type" },
+                values: new object[,]
+                {
+                    { 1, 3, "Headphones" },
+                    { 2, 3, "Keyboard" },
+                    { 3, 3, "Mouse" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Workplaces",
+                columns: new[] { "Id", "Floor", "Room", "Table" },
+                values: new object[,]
+                {
+                    { 1, 0, 1, 1 },
+                    { 2, 0, 1, 2 },
+                    { 3, 0, 2, 1 },
+                    { 4, 1, 1, 1 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "EquipmentForWorkplaces",
+                columns: new[] { "Id", "IdEquipment", "IdWorkplace" },
+                values: new object[,]
+                {
+                    { 1, 1, 1 },
+                    { 2, 2, 1 },
+                    { 3, 1, 2 },
+                    { 4, 2, 2 },
+                    { 5, 1, 3 },
+                    { 6, 2, 4 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "EquipmentForWorkplaces",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "EquipmentForWorkplaces",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "EquipmentForWorkplaces",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "EquipmentForWorkplaces",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "EquipmentForWorkplaces",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "EquipmentForWorkplaces",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Equipments",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Equipments",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Equipments",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Workplaces",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Workplaces",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Workplaces",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Workplaces",
+                keyColumn: "Id",
+                keyValue: 4);
+        }
+    }
+}
